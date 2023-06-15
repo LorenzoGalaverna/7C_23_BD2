8e6401920c5026f2a376c49e809f702ef4a5de2a
LorenzoGalaverna <lorenzo.galaverna1@gmail.com>
2023-06-15 08:23:02 -0300

    nazi

--- classes/class11.sql
+++ classes/class11.sql
@@ -23,8 +23,8 @@ from film as f
 where not exists(select i.film_id from inventory as i where i.film_id = f.film_id);
 
 select f.title, i2.inventory_id
-from film as f
-         inner join inventory i2 on f.film_id = i2.film_id
+from film film_i
+inner join inventory i2 on f.film_id = i2.film_id
 where exists(select i.film_id
              from inventory as i
              where i.film_id = f.film_id
@@ -39,7 +39,7 @@ inner join film f on i.film_id = f.film_id
 where exists(select r.customer_id from rental as r where c.customer_id = r.customer_id and r.return_date is not null);
 
 
-select s.store_id, sum(p.amount) as `total_sales($)`
+select s.store_id, sum(p.amount) as total_sales
 from store as s
          inner join inventory i on s.store_id = i.store_id
          inner join rental r on i.inventory_id = r.inventory_id
